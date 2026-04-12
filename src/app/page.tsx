0f3a2b58d15a08630b2969cec14733e40250a86d
İsmail Samet AKTEPE <ismail.aktepe0671@gmail.com>
2026-04-12 21:30:24 +0300

    ismail email deve merge edilecek

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,8 @@
 // 5. satırda bir test yapıldı conflict için eklenecek. 
 
 //  ercanın mergeinden sonra benim kodlarım gidecekmi sağlaması 
+
+//ismail e mail branchindeyken test
 import type { Metadata } from "next";
 import HomePageClient from "./page-client";
 import JsonLd from "@/components/JsonLd";
