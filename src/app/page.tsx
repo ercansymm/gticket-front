d7dc1f5d3c1b688430ed913a8e4712e36c41be94
İsmail Samet AKTEPE <ismail.aktepe0671@gmail.com>
2026-04-12 19:13:17 +0300

    test2: 5. satır conflict için test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@
 
 //burada test 1 de herhangi bir şey eklemedik bakalım sadece burası eklenecek mi var olan hangi şeyi silecek yada silmeyecek
 // İsmail test commiti - bu satır PR testi için eklendi
-
+// 5. satırda bir test yapıldı conflict için eklenecek. 
 import type { Metadata } from "next";
 import HomePageClient from "./page-client";
 import JsonLd from "@/components/JsonLd";
