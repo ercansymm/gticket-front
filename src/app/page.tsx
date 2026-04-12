205a38214dc6d89a4fbb4cc66ee690e339dcdfc7
İsmail Samet AKTEPE <ismail.aktepe0671@gmail.com>
2026-04-12 21:33:34 +0300

    ercan bey doğru kjod yazıldı tekrar kontrol edermisiniz

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@
 // 5. satırda bir test yapıldı conflict için eklenecek. 
 
 //  ercanın mergeinden sonra benim kodlarım gidecekmi sağlaması 
+//doğrusunu yazdım leaderim
 
 //ismail e mail branchindeyken test
 import type { Metadata } from "next";
